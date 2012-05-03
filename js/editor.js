mozilla/friendlycode

commit 46b8f4e6ad8bcf500fc24b8dc55848cae4a3e8bd

Replaced hacky mark logic with a new MarkTracker class, which makes the code easier to understand.

js/editor.js
@@ -4,9 +4,11 @@ var editor;
 // Provides context-sensitive help based on an index in HTML source code.
 var helpIndex = Help.Index();
 
-// Keep track of CodeMirror2 mark objects corresponding to help
-// highlighting.
-var cursorHelpMarks = [];
+// Keep track of context-sensitive help highlighting.
+var cursorHelpMarks = null;
+
+// Keep track of error highlighting.
+var errorHelpMarks = null;
 
 // Select the given {start,end} interval in the editor.
 function selectInterval(interval) {
@@ -32,31 +34,10 @@ function getIndexFromPos(editor, pos) {
 
 // Report the given Slowparse error.
 function reportError(error) {
-  $(".error").fillError(error).eachErrorHighlight(setErrorHighlight);
-  $(".help").hide();
-}
-
-// Assuming "this" is an element with a data-highlight attribute,
-// give the highlighted text interval in the editor a numbered error
-// highlight class.
-function setErrorHighlight(start, end, i) {
-  var className = "highlight-" + (i+1);
-  var start = editor.coordsFromIndex(start);
-  var end = editor.coordsFromIndex(end);
-  var mark = editor.markText(start, end, className);
-  $(this).addClass(className).data("mark", mark);
-}
-
-// Remove all highlights made by setErrorHighlight().
-function clearErrorHighlights() {
-  $(".error").eachErrorHighlight(function() {
-    // Odd, from the CodeMirror docs you'd think this would remove
-    // the class from the highlighted text, too, but it doesn't.
-    // I guess we're just garbage collecting here.
-    $(this).data("mark").clear();
+  $(".error").fillError(error).eachErrorHighlight(function(start, end, i) {
+    errorHelpMarks.mark(start, end, "highlight-" + (i+1));
   });
-  for (var i = 1; i <= 5; i++)
-    $(".CodeMirror .highlight-" + i).removeClass("highlight-" + i);
+  $(".help").hide();
 }
 
 // Update the preview area with the given HTML.
@@ -73,7 +54,7 @@ function onChange() {
   var html = editor.getValue();
   var result = Slowparse.HTML(document, html, [TreeInspectors.forbidJS]);
   helpIndex.clear();
-  clearErrorHighlights();
+  errorHelpMarks.clear();
   if (result.error)
     reportError(result.error);
   else {
@@ -88,27 +69,51 @@ function onChange() {
 
 // Called whenever the user moves their cursor in the editor area.
 function onCursorActivity() {
-  $(".CodeMirror .highlight").removeClass("cursor-help-highlight");
-  cursorHelpMarks.forEach(function(mark) {
-    // Odd, from the CodeMirror docs you'd think this would remove
-    // the class from the highlighted text, too, but it doesn't.
-    // I guess we're just garbage collecting here.
-    mark.clear();
-  });
-  cursorHelpMarks = [];
+  cursorHelpMarks.clear();
   var help = helpIndex.get(getIndexFromPos(editor, editor.getCursor()));
   if (help) {
     var learn = $("#templates .learn-more").clone()
       .attr("href", help.url);
     $(".help").html(help.html).append(learn).show();
     help.highlights.forEach(function(interval) {
-      var start = editor.coordsFromIndex(interval.start);
-      var end = editor.coordsFromIndex(interval.end);
-      var mark = editor.markText(start, end, "cursor-help-highlight");
-      cursorHelpMarks.push(mark);
+      cursorHelpMarks.mark(interval.start, interval.end,
+                           "cursor-help-highlight");
     });
   } else
     $(".help").hide();
+}
+
+// This helper class keeps track of different kinds of highlighting in
+// the editor.
+function MarkTracker(editor) {
+  var classNames = {};
+  var marks = [];
+
+  return {
+    // Mark a given start/end interval in the editor, based on character
+    // indices (not {line, ch} objects), with the given class name.
+    mark: function(start, end, className) {
+      if (!(className in classNames))
+        classNames[className] = true;
+      start = editor.coordsFromIndex(start);
+      end = editor.coordsFromIndex(end);
+      marks.push(editor.markText(start, end, className));
+    },
+    // Clear all marks made so far.
+    clear: function() {
+      marks.forEach(function(mark) {
+        // Odd, from the CodeMirror docs you'd think this would remove
+        // the class from the highlighted text, too, but it doesn't.
+        // I guess we're just garbage collecting here.
+        mark.clear();
+      });
+      for (var className in classNames)
+        $("." + className, editor.getWrapperElement()).removeClass(className);
+
+      marks = [];
+      classNames = {};
+    }
+  };
 }
 
 $(window).load(function() {
@@ -131,6 +136,8 @@ $(window).load(function() {
       },
       onCursorActivity: onCursorActivity
     });
+    cursorHelpMarks = MarkTracker(editor);
+    errorHelpMarks = MarkTracker(editor);
     editor.focus();
     onChange();
     onCursorActivity();

commit ea226a878baeb7b75e828b8d74d070fd2b6c26a1

Fixed error message highlighting of word 'here' broken by 46b8f4e6ad8bcf500fc24b8dc55848cae4a3e8bd.

js/editor.js
@@ -68,15 +68,21 @@ function MarkTracker(codeMirror) {
 
   return {
     // Mark a given start/end interval in the CodeMirror, based on character
-    // indices (not {line, ch} objects), with the given class name.
-    mark: function(start, end, className) {
+    // indices (not {line, ch} objects), with the given class name. If
+    // an element is provided, give it the class name too.
+    mark: function(start, end, className, element) {
       if (!(className in classNames))
-        classNames[className] = true;
+        classNames[className] = [];
+      if (element) {
+        classNames[className].push(element);
+        $(element).addClass(className);
+      }
       start = codeMirror.coordsFromIndex(start);
       end = codeMirror.coordsFromIndex(end);
       marks.push(codeMirror.markText(start, end, className));
     },
-    // Clear all marks made so far.
+    // Clear all marks made so far and remove the class from any elements
+    // it was previously given to.
     clear: function() {
       marks.forEach(function(mark) {
         // Odd, from the CodeMirror docs you'd think this would remove
@@ -85,9 +91,13 @@ function MarkTracker(codeMirror) {
         mark.clear();
       });
       var wrapper = codeMirror.getWrapperElement();
-      for (var className in classNames)
+      for (var className in classNames) {
+        classNames[className].forEach(function(element) {
+          $(element).removeClass(className);
+        });
         $("." + className, wrapper).removeClass(className);
-
+      }
+      
       marks = [];
       classNames = {};
     }
@@ -145,7 +155,7 @@ function ErrorHelp(options) {
   // Report the given Slowparse error.
   function reportError(error) {
     errorArea.fillError(error).eachErrorHighlight(function(start, end, i) {
-      errorHelpMarks.mark(start, end, "highlight-" + (i+1));
+      errorHelpMarks.mark(start, end, "highlight-" + (i+1), this);
     });
   }
   