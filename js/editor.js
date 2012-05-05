7988ecc3263193b9cdc16b5b55611add03c62e9a
Atul Varma <varmaa@gmail.com>
2012-05-05 10:46:36 -0400

    Fixed #4, "Preview area's scroll position always resets to top-left of page when content is changed".

--- js/editor.js
+++ js/editor.js
@@ -178,9 +178,19 @@ function LivePreview(options) {
     if (!event.error || options.ignoreErrors) {
       // Update the preview area with the given HTML.
       var doc = options.previewArea.contents()[0];
+      var wind = doc.defaultView;
+      var x = wind.pageXOffset;
+      var y = wind.pageYOffset;
+
       doc.open();
       doc.write(event.sourceCode);
       doc.close();
+
+      // TODO: If the document has images that take a while to load
+      // and the previous scroll position of the document depends on
+      // their dimensions being set on load, we may need to refresh
+      // this scroll position after the document has loaded.
+      wind.scroll(x, y);
     }
   });
   
