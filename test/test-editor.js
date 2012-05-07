0bbd2a237eee84028ecd8028d5f95ec35447ec07
Atul Varma <varmaa@gmail.com>
2012-05-07 13:38:38 -0400

    split out ParsingCodeMirror tests into multiple different tests

--- test/test-editor.js
+++ test/test-editor.js
@@ -28,82 +28,104 @@
   }));
 })();
 
-module("Editor");
-
-test("ParsingCodeMirror works", function() {
-  var place = $("<div></div>").appendTo(document.body);
-  var events = [];
-  var fakeTime = {
-    id: 0,
-    setTimeout: function(cb, ms) {
-      this.cb = cb;
-      events.push("time.setTimeout(fn, " + ms + ") -> " + this.id);
-      return this.id++;
-    },
-    clearTimeout: function(id) {
-      events.push("time.clearTimeout(" + id + ")");
-    }
-  };
-  var cm = ParsingCodeMirror(place[0], {
-    mode: "text/plain",
-    parseDelay: 1,
-    parse: function(code) {
-      return {
-        error: "here is an error",
-        document: "here is a document"
-      };
-    },
-    time: fakeTime
-  });
-  cm.on("all", function(eventName, arg) {
-    events.push("cm.trigger('" + eventName + "')");
-  });
-
-  cm.setValue("hello");
-  deepEqual(events, [
-    "cm.trigger('change')",
-    "time.setTimeout(fn, 1) -> 0",
-  ], "change event is triggered and timeout function is set");
-  events = [];
-
-  cm.on("reparse", function(arg) {
-    equal(arg.document, "here is a document", "document passed on reparse");
-    equal(arg.error, "here is an error", "error passed on reparse");
-    equal(arg.sourceCode, "hello", "source code passed on reparse");
-  });
-  cm.reparse();
-  deepEqual(events, [
-    "cm.trigger('reparse')",
-    "cm.trigger('cursor-activity')",
-  ], "reparse and cursor activity events are triggered");
-  cm.off("reparse");
-  events = [];
-
-  cm.setValue("hello goober");
-  deepEqual(events, [
-    "cm.trigger('change')",
-    "time.clearTimeout(0)",
-    "time.setTimeout(fn, 1) -> 1"
-  ], "old timeout function is cleared");
-  events = [];
-
-  cm.on("reparse", function(event) {
-    equal(event.sourceCode, "hello goober",
-          "correct source code is passed on reparse event");
-  });
-  fakeTime.cb();
-  deepEqual(events, [
-    "cm.trigger('reparse')",
-    "cm.trigger('cursor-activity')"
-  ], "reparse/cursor-activity events are triggered by timeout function");
-  events = [];
+(function() {
+  module("ParsingCodeMirror");
   
-  cm.setCursor({line: 0, ch: 2});
-  deepEqual(events, ["cm.trigger('cursor-activity')"],
-            "cursor-activity event is triggered by setCursor()");
+  function pcmTest(cb) {
+    return function() {
+      var place = $("<div></div>").appendTo(document.body);
+      var events = [];
+      var fakeTime = {
+        id: 0,
+        setTimeout: function(cb, ms) {
+          this.cb = cb;
+          events.push("time.setTimeout(fn, " + ms + ") -> " + this.id);
+          return this.id++;
+        },
+        clearTimeout: function(id) {
+          events.push("time.clearTimeout(" + id + ")");
+        }
+      };
+      var cm = ParsingCodeMirror(place[0], {
+        mode: "text/plain",
+        parseDelay: 1,
+        parse: function(code) {
+          return {
+            error: "here is an error",
+            document: "here is a document"
+          };
+        },
+        time: fakeTime
+      });
+      cm.on("all", function(eventName, arg) {
+        events.push("cm.trigger('" + eventName + "')");
+      });
+      try {
+        cb(cm, events, fakeTime);
+      } finally {
+        place.remove();
+      }
+    };
+  }
+  
+  test("change triggered and timeout set on codeMirror.setValue()",
+       pcmTest(function(cm, events, fakeTime) {
+         cm.setValue("hello");
+         deepEqual(events, [
+           "cm.trigger('change')",
+           "time.setTimeout(fn, 1) -> 0",
+         ]);
+       }));
 
-  place.remove();
-});
+  test("reparse() triggers events and passes expected arguments",
+       pcmTest(function(cm, events, fakeTime) {
+         cm.setValue("hello"); events.splice(0);
+         cm.on("reparse", function(arg) {
+           equal(arg.document, "here is a document", "document passed");
+           equal(arg.error, "here is an error", "error passed");
+           equal(arg.sourceCode, "hello", "source code passed");
+         });
+         cm.reparse();
+         deepEqual(events, [
+           "cm.trigger('reparse')",
+           "cm.trigger('cursor-activity')",
+          ]);
+       }));
+  
+  test("old timeout cancelled on multiple content changes",
+       pcmTest(function(cm, events, fakeTime) {
+         cm.setValue("hello"); events.splice(0);
+         cm.setValue("hello goober");
+         deepEqual(events, [
+           "cm.trigger('change')",
+           "time.clearTimeout(0)",
+           "time.setTimeout(fn, 1) -> 1"
+         ]);
+       }));
+
+  test("timeout function triggers events w/ expected args",
+       pcmTest(function(cm, events, fakeTime) {
+         cm.setValue("hello goober"); events.splice(0);
+         cm.on("reparse", function(event) {
+           equal(event.sourceCode, "hello goober",
+                 "correct source code is passed on reparse event");
+         });
+         fakeTime.cb();
+         deepEqual(events, [
+           "cm.trigger('reparse')",
+           "cm.trigger('cursor-activity')"
+         ], "events are triggered");
+       }));
+
+  test("cursor-activity event is triggered by codeMirror.setCursor()",
+       pcmTest(function(cm, events, fakeTime) {
+         cm.setValue("hello"); events.splice(0);
+         cm.setCursor({line: 0, ch: 2});
+         deepEqual(events, ["cm.trigger('cursor-activity')"]);
+       }));
+})();
+
+module("Editor");
 
 test("MarkTracker works", function() {
   var place = $("<div></div>").appendTo(document.body);
