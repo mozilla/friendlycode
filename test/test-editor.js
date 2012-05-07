28fa39e7ce0e67e401f3b7fbb041bdfaa6195d83
Atul Varma <varmaa@gmail.com>
2012-05-07 11:26:46 -0400

    added tests for MarkTracker

--- test/test-editor.js
+++ test/test-editor.js
@@ -84,3 +84,23 @@ test("ParsingCodeMirror works", function() {
 
   place.remove();
 });
+
+test("MarkTracker works", function() {
+  var place = $("<div></div>").appendTo(document.body);
+  var cm = CodeMirror(place[0], {mode: "text/plain"});
+  var mt = MarkTracker(cm);
+  
+  cm.setValue("hello");
+  mt.mark(2, 4, "blah");
+  equal(place.find(".blah").text(), "ll", "source code is marked w/ class");
+  mt.clear();
+  equal(place.find(".blah").length, 0, "source code class is cleared");
+
+  var thing = $("<div></div>");
+  mt.mark(1, 4, "foo", thing[0]);
+  ok(thing.hasClass("foo"), "related element is marked w/ class");
+  mt.clear();
+  ok(!thing.hasClass("foo"), "related element class is cleared");
+
+  place.remove();
+});
