00b7c88a7a7653cca8c03f81235592ad6cf987bd
Atul Varma <varmaa@gmail.com>
2012-05-24 21:15:14 -0400

    Removed getQueryVariable() as it's no longer needed.

--- js/main.js
+++ js/main.js
@@ -21,17 +21,6 @@
 // purposes. Other parts of our code should never cite this module
 // as a dependency.
 define("main", function(require) {
-  function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] == variable) {
-        return unescape(pair[1]);
-      }
-    }
-  }
-
   var Help = require("fc/help"),
       Parachute = require("fc/parachute"),
       Publisher = require("fc/publisher"),
