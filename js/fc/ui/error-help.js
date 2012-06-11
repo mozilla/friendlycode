b12073818235c0cb7bbbbc3e3b908449e1457a6b
Atul Varma <varmaa@gmail.com>
2012-06-11 10:10:51 -0400

    Much better indication for error targets ("here" labels).
    Specifically, the first "here" is not highlighted at all because
    the error message is pointing at it. The second "here" appears
    hyperlinked, and if the user clicks on it, the cursor position
    is set to the location, and an animated red arrow appears to
    guide the user's eye towards it.

--- js/fc/ui/error-help.js
+++ js/fc/ui/error-help.js
@@ -3,6 +3,25 @@
 // Provides helpful Slowparse-based error suggestions for a
 // ParsingCodeMirror.
 define(["./mark-tracker"], function(MarkTracker) {
+  // Display an animated arrow pointing at a particular position in a
+  // codeMirror instance. It disappears after a short delay.
+  function pointAtPosition(codeMirror, pos) {
+    var MOVE_TIME = 500;
+    var PAUSE_TIME = 1000;
+    var pointer = $('<div class="cursor-pointer"></div>');
+    codeMirror.addWidget(pos, pointer[0], true);
+    pointer.css({
+      opacity: 0,
+      paddingTop: '50px'
+    });
+    pointer.animate({
+      opacity: 1,
+      paddingTop: 0
+    }, MOVE_TIME).delay(PAUSE_TIME).fadeOut(function() {
+      pointer.remove();
+    });
+  }
+
   return function ErrorHelp(options) {
     var self = {};
     var codeMirror = options.codeMirror;
@@ -16,15 +35,20 @@ define(["./mark-tracker"], function(MarkTracker) {
     // Report the given Slowparse error.
     function reportError(error) {
       var startMark = null;
-      var errorHTML = $("<div></div>").fillError(error)
-        .eachErrorHighlight(function(start, end, i) {
-          // Point the error message's arrow at the first occurrence of
-          // the word "here" in the error message.
-          if (startMark === null)
-            startMark = start;
-          errorHelpMarks.mark(start, end, "highlight-" + (i+1), this);
-        });
+      var errorHTML = $("<div></div>").fillError(error);
       errorArea.html(template({error: errorHTML.html()})).show();
+      errorArea.eachErrorHighlight(function(start, end, i) {
+        // Point the error message's arrow at the first occurrence of
+        // the word "here" in the error message.
+        if (startMark === null)
+          startMark = start;
+        errorHelpMarks.mark(start, end, "highlight-" + (i+1), this);
+        $(this).click(function() {
+          var pos = codeMirror.posFromIndex(start);
+          codeMirror.setCursor(pos);
+          pointAtPosition(codeMirror, pos);
+        });
+      });
       relocator.relocate(errorArea, startMark, "error");
     }
   
