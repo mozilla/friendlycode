0066dcf44062d7abcb6b1fa8449833eb2769af31
smagch <smagch@gmail.com>
2012-06-29 17:16:40 +0900

    make template plugin optimizable

--- js/template.js
+++ js/template.js
@@ -1,8 +1,32 @@
 // This is a simple RequireJS plugin that loads an underscore.js template.
-define({
-  load: function(name, req, load, config) {
-    req(["text!../templates/" + name + ".html"], function(text) {
-      load(_.template(text));
-    });
-  }
+define(["module", "text", "underscore"], function (module, text, _) {
+  var buildMap = {},
+      masterConfig = module.config();
+
+  return {
+    load: function(name, req, onLoad, config) {
+      var url = req.toUrl(name).replace(".js", ".html");
+
+      text.get("templates/" + name + ".html", function (data) {
+        // remove break lines
+        data = data.replace(/\r\n|\n|\r/g, '');
+
+        var template;
+        if (config.isBuild) {
+          template = buildMap[name] = "_.template('" + data  + "')";
+        } else {
+          template = _.template(data);
+        }
+
+        onLoad(template);
+      });
+    },
+    write: function (pluginName, moduleName, write) {
+      if (buildMap[moduleName]) {
+        var content = buildMap[moduleName];
+        write.asModule(pluginName + "!" + moduleName,
+          "define(function () { \n  return " + content + ";});\n");
+      }
+    }
+  };
 });
