77888dd327d0fa4230baefc860b2efb363e13fff
Atul Varma <varmaa@gmail.com>
2012-07-05 09:11:19 -0400

    Consolidated duplicate requirejs config into js/require-config.js.
    This file can be loaded both as a browser script *BEFORE* requirejs, and
    as a module from node.

--- build-require.js
+++ build-require.js
@@ -7,21 +7,17 @@
  */
 var requirejs = require('requirejs'),
   resolve = require('path').resolve,
+  requireConfig = require('./js/require-config'),
   baseUrl = resolve(__dirname, 'js'),
   name = 'main',
   out = resolve(baseUrl, 'main-built.js'),
   mainConfigFile = resolve(baseUrl, 'main.js');
 
-optimize(function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("build success");
-  }
-  process.exit();
-});
-
 function optimize(done) {
+  requirejs.optimize(generateConfig(), done);
+}
+
+function generateConfig() {
   var config = {
     name: name,
     out: out,
@@ -36,63 +32,36 @@ function optimize(done) {
     // TODO  above config setting is temporary, it shuould use mainConfigFile
     // https://github.com/toolness/friendlycode/pull/112#issuecomment-6625412
     // mainConfigFile: "./js/main.js",
-    baseUrl: "js",
-    shim: {
-      underscore: {
-        exports: "_"
-      },
-      jquery: {
-        exports: "$"
-      },
-      "jquery-tipsy": {
-        deps: ["jquery"],
-        exports: "$"
-      },
-      "jquery-slowparse": {
-        deps: ["jquery"],
-        exports: "$"
-      },
-      backbone: {
-        deps: ["underscore", "jquery"],
-        exports: "Backbone"
-      },
-      codemirror: {
-        exports: "CodeMirror"
-      },
-      "codemirror/xml": {
-        deps: ["codemirror"],
-        exports: "CodeMirror"
-      },
-      "codemirror/javascript": {
-        deps: ["codemirror"],
-        exports: "CodeMirror"
-      },
-      "codemirror/css": {
-        deps: ["codemirror"],
-        exports: "CodeMirror"
-      },
-      "codemirror/html": {
-        deps: [
-          "codemirror/xml",
-          "codemirror/javascript",
-          "codemirror/css"
-        ],
-        exports: "CodeMirror"
-      }
-    },
-    paths: {
-      jquery: "jquery.min",
-      "jquery-tipsy": "jquery.tipsy",
-      "jquery-slowparse": "../slowparse/spec/errors.jquery",
-      underscore: "underscore.min",
-      backbone: "backbone.min",
-      slowparse: "../slowparse",
-      codemirror: "../codemirror2/lib/codemirror",
-      "codemirror/xml": "../codemirror2/mode/xml/xml",
-      "codemirror/javascript": "../codemirror2/mode/javascript/javascript",
-      "codemirror/css": "../codemirror2/mode/css/css",
-      "codemirror/html": "../codemirror2/mode/htmlmixed/htmlmixed"
-    }
   };
-  requirejs.optimize(config, done);
+  Object.keys(requireConfig).forEach(function(name) {
+    config[name] = requireConfig[name];
+  });
+  return config;
+}
+
+function selfTest() {
+  var assert = require('assert'),
+      config = generateConfig();
+      
+  assert('paths' in config);
+  assert('jquery' in config.shim);
+}
+
+if (!module.parent) {
+  if (process.argv[2] == 'test') {
+    selfTest();
+    console.log("Tests pass!");
+    process.exit(0);
+  } else {
+    console.log("Generating", out);
+
+    optimize(function (err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("build success");
+      }
+      process.exit();
+    });
+  }
 }
---
+++ js/require-config.js
@@ -0,0 +1,63 @@
+var require = {
+  baseUrl: "js",
+  shim: {
+    underscore: {
+      exports: "_"
+    },
+    jquery: {
+      exports: "$"
+    },
+    "jquery-tipsy": {
+      deps: ["jquery"],
+      exports: "$"
+    },
+    "jquery-slowparse": {
+      deps: ["jquery"],
+      exports: "$"
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    },
+    codemirror: {
+      exports: "CodeMirror"
+    },
+    "codemirror/xml": {
+      deps: ["codemirror"],
+      exports: "CodeMirror"
+    },
+    "codemirror/javascript": {
+      deps: ["codemirror"],
+      exports: "CodeMirror"
+    },
+    "codemirror/css": {
+      deps: ["codemirror"],
+      exports: "CodeMirror"
+    },
+    "codemirror/html": {
+      deps: [
+        "codemirror/xml",
+        "codemirror/javascript",
+        "codemirror/css"
+      ],
+      exports: "CodeMirror"
+    }
+  },
+  paths: {
+    jquery: "jquery.min",
+    "jquery-tipsy": "jquery.tipsy",
+    "jquery-slowparse": "../slowparse/spec/errors.jquery",
+    underscore: "underscore.min",
+    backbone: "backbone.min",
+    slowparse: "../slowparse",
+    codemirror: "../codemirror2/lib/codemirror",
+    "codemirror/xml": "../codemirror2/mode/xml/xml",
+    "codemirror/javascript": "../codemirror2/mode/javascript/javascript",
+    "codemirror/css": "../codemirror2/mode/css/css",
+    "codemirror/html": "../codemirror2/mode/htmlmixed/htmlmixed",
+    test: "../test"
+  }
+};
+
+if (typeof(module) == 'object' && module.exports)
+  module.exports = require;
