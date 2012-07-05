mozilla/friendlycode

commit 105353514bb110cfad9bd8ac3f59b16a430fd4a3

jQuery.noConflict() wasn't being called before; now it is.
I'm not sure why we have to go through requirejs' 'map' config
option rather than its 'path' config option to do this, but we do.

Also fixed a number of tests that were using the $ global instead
of an injected dependency.

js/require-config.js
@@ -6,7 +6,7 @@ var require = {
         return _.noConflict();
       }
     },
-    jquery: {
+    "jquery.min": {
       exports: function() {
         jQuery.noConflict();
         return jQuery;
@@ -48,8 +48,12 @@ var require = {
       exports: "CodeMirror"
     }
   },
+  map: {
+    "*": {
+      jquery: "jquery.min"
+    }
+  },
   paths: {
-    jquery: "jquery.min",
     "jquery-tipsy": "jquery.tipsy",
     "jquery-slowparse": "../slowparse/spec/errors.jquery",
     underscore: "underscore.min",

test/test-app.js
@@ -35,7 +35,7 @@ require(["jquery", "lscache"], function($, lscache) {
   });
   
   appTest("publish works", function(window, start) {
-    var $ = window.jQuery;
+    var $ = window.require("jquery");
     var publishURL = $('meta[name="publish-url"]').attr("content");
     var url = window.location.href;
     

js/fc/publisher.js
@@ -2,7 +2,7 @@
 
 // This class is responsible for communicating with a publishing server
 // to save and load published code.
-define(function() {
+define(["jquery"], function($) {
   var myOrigin = window.location.protocol + "//" + window.location.host;
   
   function Publisher(baseURL) {
@@ -14,7 +14,7 @@ define(function() {
       if (baseURL == myOrigin)
         return path;
       path = baseURL + path;
-      if (!jQuery.support.cors && window.console)
+      if (!$.support.cors && window.console)
         window.console.warn("No CORS detected for request to " + path);
       return path;
     }
@@ -22,7 +22,7 @@ define(function() {
     return {
       baseURL: baseURL,
       loadCode: function(path, cb) {
-        jQuery.ajax({
+        $.ajax({
           type: 'GET',
           url: makeURL(path),
           dataType: 'text',

test/codemirror-577/test-codemirror-577.js
@@ -1,10 +1,11 @@
 "use strict";
 
 require([
+  "jquery",
   "codemirror",
   "text!test/codemirror-577/original.html",
   "text!test/codemirror-577/replacer.html"
-], function(CodeMirror, original, replacer) {
+], function($, CodeMirror, original, replacer) {
   module("Codemirror issue 577");
   
   // Test to ensure that this bug is fixed or worked around:

test/preview-to-editor-mapping/test-preview-to-editor-mapping.js
@@ -1,8 +1,9 @@
 require([
+  "jquery",
   "fc/ui/preview-to-editor-mapping",
   "test/test-live-preview",
   "text!test/preview-to-editor-mapping/path-to.html"
-], function(PreviewToEditorMapping, testLivePreview, pathToHTML) {
+], function($, PreviewToEditorMapping, testLivePreview, pathToHTML) {
   module("PreviewToEditorMapping");
   
   var nodeToCode = PreviewToEditorMapping._nodeToCode;

test/test-indexable-codemirror.js
@@ -1,6 +1,9 @@
 "use strict";
 
-require(["fc/ui/indexable-codemirror"], function(IndexableCodeMirror) {
+require([
+  "jquery",
+  "fc/ui/indexable-codemirror"
+], function($, IndexableCodeMirror) {
   module("IndexableCodeMirror");
   
   function icmTest(name, cb) {

test/test-live-preview.js
@@ -1,10 +1,11 @@
 "use strict";
 
 define([
+  "jquery",
   "underscore",
   "fc/ui/live-preview",
   "slowparse/slowparse"
-], function(_, LivePreview, Slowparse) {
+], function($, _, LivePreview, Slowparse) {
   module("LivePreview");
   
   function lpTest(name, html, cb) {

test/test-mark-tracker.js
@@ -1,9 +1,10 @@
 "use strict";
 
 require([
+  "jquery",
   "fc/ui/mark-tracker",
   "codemirror"
-], function(MarkTracker, CodeMirror) {
+], function($, MarkTracker, CodeMirror) {
   module("MarkTracker");
   
   function mtTest(name, cb) {

commit a037d23125ea72d020d00173e298ae4cd3ae067e

Using require.deps and require.callback instead of require.map for jquery.
Commit 105353514bb110cfad9bd8ac3f59b16a430fd4a3 broke build-require.js.
But it looks like we can use deps and callback to do the same thing.

js/require-config.js
@@ -6,12 +6,9 @@ var require = {
         return _.noConflict();
       }
     },
-    "jquery.min": {
-      exports: function() {
-        jQuery.noConflict();
-        return jQuery;
-      }
-    },
+    // We would have an entry for jquery in here, but requirejs never
+    // seems to consult it, perhaps because define.amd contains 'jQuery'
+    // in require.min.js.
     "jquery-tipsy": {
       deps: ["jquery"],
       exports: 'jQuery'
@@ -48,12 +45,8 @@ var require = {
       exports: "CodeMirror"
     }
   },
-  map: {
-    "*": {
-      jquery: "jquery.min"
-    }
-  },
   paths: {
+    jquery: "jquery.min",
     "jquery-tipsy": "jquery.tipsy",
     "jquery-slowparse": "../slowparse/spec/errors.jquery",
     underscore: "underscore.min",
@@ -65,6 +58,13 @@ var require = {
     "codemirror/css": "../codemirror2/mode/css/css",
     "codemirror/html": "../codemirror2/mode/htmlmixed/htmlmixed",
     test: "../test"
+  },
+  deps: ['jquery'],
+  callback: function($) {
+    // This really ought to be called from shim.jquery.exports(),
+    // but as mentioned above, requirejs doesn't seem to ever consult
+    // shim.jquery, so we'll do it here.
+    $.noConflict();
   }
 };
 

test/test-app.js
@@ -26,10 +26,11 @@ require(["jquery", "lscache"], function($, lscache) {
   }
   
   appTest("navbar shows page title", function(window, start) {
+    var $ = window.require("jquery");
     var cm = window.require("main").codeMirror;
     cm.setValue("<title>supdog</title>");
     cm.reparse();
-    equal(window.$("#nav-options .preview-title").text(), "supdog",
+    equal($("#nav-options .preview-title").text(), "supdog",
           "navbar preview title is 'supdog'");
     start();
   });