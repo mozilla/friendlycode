d0219212729fd750abff30856a5a1496a59d34f0
Atul Varma <varmaa@gmail.com>
2012-08-18 00:44:24 -0400

    added test suite for current-page-manager.

--- js/fc/current-page-manager.js
+++ js/fc/current-page-manager.js
@@ -1,13 +1,22 @@
 define(function() {
+  function defaultLoadPage(window) {
+    // We don't currently support dynamically changing the URL
+    // without a full page reload, unfortunately, so just trigger a
+    // reload if the user clicked the 'back' button after we pushed
+    // a new URL to it.    
+    return function() { window.location.reload() };
+  }
+  
   return function CurrentPageManager(options) {
     var self = {},
         window = options.window,
         pageToLoad = options.currentPage,
+        loadPage = options.loadPage || defaultLoadPage(window),
         supportsPushState = window.history.pushState ? true : false;
     
     if (supportsPushState)
       window.history.replaceState({pageToLoad: pageToLoad}, "",
-                                  location.href);
+                                  window.location.href);
     
     // If a URL hash is specified, it should override anything provided by
     // a server.
@@ -15,27 +24,22 @@ define(function() {
       pageToLoad = window.location.hash.slice(1);
     
     window.addEventListener("hashchange", function(event) {
-      // We don't currently support dynamically changing the URL
-      // without a full page reload, unfortunately, so just trigger a
-      // reload if the user clicked the 'back' button after we pushed
-      // a new URL to it.
       var newPageToLoad = window.location.hash.slice(1);
-      if (newPageToLoad != pageToLoad)
-        window.location.reload();
+      if (newPageToLoad != pageToLoad) {
+        pageToLoad = newPageToLoad;
+        loadPage(pageToLoad);
+      }
     }, false);
 
     if (supportsPushState)
       window.addEventListener("popstate", function(event) {
-        // We don't currently support dynamically changing the URL
-        // without a full page reload, unfortunately, so just trigger a
-        // reload if the user clicked the 'back' button after we pushed
-        // a new URL to it.
-        //
-        // Also, for some reason Webkit is sending a spurious popstate with
+        // For some reason Webkit is sending a spurious popstate with
         // state == null on page load, so we want to check that it's
         // non-null first (see #39).
-        if (event.state && event.state.pageToLoad != pageToLoad)
-          window.location.reload();
+        if (event.state && event.state.pageToLoad != pageToLoad) {
+          pageToLoad = event.state.pageToLoad;
+          loadPage(pageToLoad);
+        }
       }, false);
     
     self.currentPage = function() { return pageToLoad; };
