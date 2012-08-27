d3a6da7bb14bf7f3dbd8335d7b846faf675ec7ee
Atul Varma <varmaa@gmail.com>
2012-08-27 10:53:01 -0400

    Initial attempt at an SVG-based gutter pointer.
    This gutter pointer is based on Jess' original mockup here:
    
      http://www.flickr.com/photos/jessicaklein/7221327312
    
    Currently this needs to be changed so the pointer's left side starts
    at the very right side of the gutter, and extends into the margin of
    the actual text area.

--- js/fc/ui/relocator.js
+++ js/fc/ui/relocator.js
@@ -2,9 +2,47 @@
 
 // code will relocate an error or help message to near where the error actually is in CodeMirror.
 define(["jquery"], function($) {
+  function makeGutterPointer(codeMirror, highlightClass) {
+    function attrs(element, attributes) {
+      for (var name in attributes)
+        element.setAttribute(name, attributes[name].toString());
+    }
+    
+    var wrapper = $(codeMirror.getWrapperElement());
+    var highlight = $(".CodeMirror-gutter-text ." + highlightClass, wrapper);
+    var SVG_NS = "http://www.w3.org/2000/svg";
+    var svg = document.createElementNS(SVG_NS, "svg");
+    var w = $(".CodeMirror-gutter", wrapper).outerWidth() -
+            highlight.width();
+    var h = highlight.height();
+    var pos = highlight.position();
+    
+    pos.left += highlight.width();
+    attrs(svg, {
+      'class': "gutter-pointer " + highlightClass,
+      viewBox: [0, 0, w, h].join(" ")
+    });
+    var pointer = document.createElementNS(SVG_NS, "polygon");
+    attrs(pointer, {
+      points: ["0,0", w + "," + (h/2), "0," + h].join(" ")
+    });
+    svg.appendChild(pointer);
+    $(svg).css({
+      width: w + "px",
+      height: h + "px",
+      top: pos.top + "px",
+      left: pos.left + "px"
+    });
+
+    $(".CodeMirror-scroll", wrapper).append(svg);
+    
+    return $(svg);
+  }
+  
   return function Relocator(codeMirror) {
     var lastPos = null;
     var lastElement = null;
+    var lastGutterPointer = null;
     var lastToggle = document.createElement("div");
 
     function flipElementIfNeeded() {
@@ -30,6 +68,10 @@ define(["jquery"], function($) {
           lastElement.hide();
           lastElement = null;
         }
+        if (lastGutterPointer) {
+          lastGutterPointer.remove();
+          lastGutterPointer = null;
+        }
         if (lastToggle.parentNode) {
           $(lastToggle).remove();
         }
@@ -37,6 +79,8 @@ define(["jquery"], function($) {
 
       // relocate an element to inside CodeMirror, pointing "at" the line for startMark
       relocate: function(element, startMark, type) {
+        var highlightClass = "gutter-highlight-" + type;
+
         this.cleanup();
         lastElement = $(element);
 
@@ -46,7 +90,8 @@ define(["jquery"], function($) {
         // the right one.
         lastPos = codeMirror.posFromIndex(startMark);
         codeMirror.setLineClass(lastPos.line, null, "CodeMirror-line-highlight");
-        codeMirror.setMarker(lastPos.line, null, "gutter-highlight-" + type);
+        codeMirror.setMarker(lastPos.line, null, highlightClass);
+        lastGutterPointer = makeGutterPointer(codeMirror, highlightClass);
 
         codeMirror.addWidget(lastPos, lastElement[0], false);
         $(".up-arrow, .down-arrow", lastElement).css({
