79c179ae8a97b858fde6c22da3356f7c09cfd467
Atul Varma <varmaa@gmail.com>
2012-12-03 13:53:28 -0500

    moved content in js/fc/nls/root/ into parent dir.

--- js/fc/nls/hacktionary.js
+++ js/fc/nls/hacktionary.js
@@ -1,3 +1,10 @@
+"use strict";
+
 define({
-  root: true
+  root: {
+    "css-selector-docs": "This is a CSS selector. It describes the kinds of HTML elements that a CSS rule's style will be applied to.",
+    "mdn_url_html": "https://developer.mozilla.org/en/HTML/Element/",
+    "mdn_url_css": "https://developer.mozilla.org/en/CSS/",
+    "mdn_url_css_selectors": "https://developer.mozilla.org/en/CSS/Getting_Started/Selectors"
+  }
 });
