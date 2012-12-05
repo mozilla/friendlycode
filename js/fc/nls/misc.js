b6d90abae1853254e4b20b1f695b68e2cce05d2f
Atul Varma <varmaa@gmail.com>
2012-12-04 19:24:10 -0500

    made fc/ui/publish.js use fc/nls/misc.

--- js/fc/nls/misc.js
+++ js/fc/nls/misc.js
@@ -3,6 +3,7 @@
 define({
   description: 'Miscellaneous strings.',
   root: {
-    "page-load-err": 'Sorry, an error occurred while trying to get the page.'
+    "page-load-err": 'Sorry, an error occurred while trying to get the page.',
+    "publish-err": 'Sorry, an error occurred while trying to publish.'
   }
 });
