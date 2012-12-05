134f64f0a3316de0641441a8f91ffaa3d92eaacd
Atul Varma <varmaa@gmail.com>
2012-12-04 19:34:13 -0500

    internationalized social-media.js.

--- js/fc/nls/misc.js
+++ js/fc/nls/misc.js
@@ -4,6 +4,9 @@ define({
   description: 'Miscellaneous strings.',
   root: {
     "page-load-err": 'Sorry, an error occurred while trying to get the page.',
-    "publish-err": 'Sorry, an error occurred while trying to publish.'
+    "publish-err": 'Sorry, an error occurred while trying to publish.',
+    "facebook-locale": "en_US",
+    "default-tweet": "Check out the #MozThimble page I just made:",
+    "tweet": "Tweet"
   }
 });
