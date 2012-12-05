aa491e0291c92b1779dec950dcc9dd0aac9961c4
Atul Varma <varmaa@gmail.com>
2012-12-04 20:19:16 -0500

    added descriptions to slowparse error i18n bundles.

--- js/slowparse-errors/nls/base.js
+++ js/slowparse-errors/nls/base.js
@@ -2,5 +2,8 @@ define([
   '../html-to-i18n-bundle',
   'text!slowparse/spec/errors.base.html'
 ], function(htmlToI18nBundle, base) {
-  return {root: htmlToI18nBundle(base)};
+  return {
+    description: '<a href="http://mozilla.github.com/slowparse/spec/">Slowparse errors</a> shown when the user makes basic mistakes like forgetting to close a tag.',
+    root: htmlToI18nBundle(base)
+  };
 });
--- js/slowparse-errors/nls/forbidjs.js
+++ js/slowparse-errors/nls/forbidjs.js
@@ -2,5 +2,8 @@ define([
   '../html-to-i18n-bundle',
   'text!slowparse/spec/errors.forbidjs.html'
 ], function(htmlToI18nBundle, forbidjs) {
-  return {root: htmlToI18nBundle(forbidjs)};
+  return {
+    description: '<a href="http://mozilla.github.com/slowparse/spec/">Slowparse errors</a> shown when the user tries to use JavaScript but it\'s not allowed.',
+    root: htmlToI18nBundle(forbidjs)
+  };
 });
