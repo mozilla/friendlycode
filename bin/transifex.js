8d8197e48c9beebc47dfdbc3558d8a8c4a438a89
Atul Varma <varmaa@gmail.com>
2013-01-23 19:41:50 -0500

    Added a	very early bin/transifex.js and	test suite.
    The script doesn't really do anything yet other	than display some
    information about the project.
    
    Also added request as a dependency to package.json, as we use it to
    communicate with transifex's REST API.

---
+++ bin/transifex.js
@@ -0,0 +1,53 @@
+const BASE_URL = 'https://www.transifex.com/api/2/project/';
+
+var request = require('request');
+
+var toBundleLocale = exports.toBundleLocale = function(locale) {
+  return locale.toLowerCase().replace(/_/g, '-');
+};
+
+var toBundleMetadata = exports.toBundleMetadata = function(resource) {
+  var metadata = {};
+  resource.available_languages.forEach(function(language) {
+    if (language.code == resource.source_language_code)
+      return metadata.root = true;
+    metadata[toBundleLocale(language.code)] = true;
+  });
+  return metadata;
+};
+
+var toBundleDict = exports.toBundleDict = function(options) {
+  var dict = {};
+  options.strings.forEach(function(info) {
+    if (!info.translation) return;
+    if (options.reviewedOnly && !info.reviewed) return;
+    dict[info.key] = info.translation;
+  });
+  return dict;
+};
+
+function main() {
+  var program = require('commander');
+  program
+    .option('-u, --user <user:pass>', 'specify username and password')
+    .option('-p, --project <slug>', 'specify project slug')
+    .parse(process.argv);
+  if (!program.user) {
+    console.log('please specify username and password.');
+    process.exit(1);
+  }
+  if (!program.project) {
+    console.log('please specify a project.');
+    process.exit(1);
+  }
+  request.get({
+    url: BASE_URL + program.project + "/?details",
+    headers: {
+      'Authorization': 'Basic ' + new Buffer(program.user).toString('base64')
+    }
+  }, function(error, response, body) {
+    console.log("RESPONSE", error, response.statusCode, body);
+  });
+}
+
+if (!module.parent) main();
